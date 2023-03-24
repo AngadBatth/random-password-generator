53981d22906be0311fb88da42abb8596e6a6fab4
AngadBatth <andyybatth@gmail.com>
2023-03-24 00:01:20 -0400

    added comments to JS file

--- Develop/script.js
+++ Develop/script.js
@@ -17,9 +17,12 @@ var upperCaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbersList = "0123456789";
 var symbolsList = "!#$%&'()*_+,-[\\]./:;=<>?@^`{|}~";
 
+
+// The function that will actually generate the Password
 function generatePassword() {
   var passLength = prompt("How many Characters would you like the password to be? (Please choose between 8 - 128 characters)");
 
+  // Makes sure Password length is not below 8 or exceeds 128
   if (passLength < 8 || passLength > 128) {
     alert("Error: Invalid input. Please choose a number between 8 - 128 characters");
     return;
@@ -32,6 +35,7 @@ function generatePassword() {
     var includeSymbol = confirm("Would you like to include special Symbols in the password?");
     var includeThese = "";
   
+    // Checking boolean values and if true, concatenating lists to includeThese variable.
     if (includeLower == true) {
       includeThese += lowerCaseList;
     }
@@ -48,6 +52,7 @@ function generatePassword() {
       includeThese += symbolsList;
     }
   
+    // Using a While loop to ensure atleast 1 type is included. Keeps user in infinite loop of prompts unless selecting 1 attribute as true
     while (!includeLower && !includeUpper && !includeNums && !includeSymbol) {
       alert("Please select 1 type to include");
   
@@ -58,6 +63,8 @@ function generatePassword() {
   
     }
   
+    // This for loops takes the given password length by the user + all the lists in the includeThese variable and then randomly generates
+    // a password with the given criteria from the User. This is done by using the Math.floor and Math.random methods
     for (i = 0; i < passLength; i++) {
       password += includeThese[Math.floor(Math.random() * includeThese.length)];
     }
